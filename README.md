f7b70b944545221bb35e758ea5577fb5f446456b
Joel Jaurgeui <joel.jauregui@gmail.com>
2012-05-09 17:16:16 -0700

    Add custom java class tag and mobile-esp dep
    + Added dependency to mobile-esp
    + Added Webhound custom tag class
    + Wrote documentation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ webhound
 --------
 
 webhound is a module for the Play Framework! (1.2.x) that leverages [MobileESP][1]
-to provide tags that help template views depending on the useragent the device browsing
+to provide tags that help template views depending on the useragent of the device browsing
 the page.
 
 [1]: http://code.google.com/p/mobileesp/
---
+++ app/views/tags/webhound/Webhound.java
@@ -0,0 +1,59 @@
+package views.tags.webhound;
+
+import com.handinteractive.mobile.UAgentInfo;
+
+import groovy.lang.Closure;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import play.exceptions.TagInternalException;
+import play.exceptions.TemplateExecutionException;
+import play.exceptions.TemplateNotFoundException;
+import play.mvc.Http;
+import play.mvc.Scope.Flash;
+import play.mvc.Scope.Session;
+import play.templates.JavaExtensions;
+import play.templates.FastTags;
+import play.templates.GroovyTemplate.ExecutableTemplate;
+import play.utils.HTML;
+
+@FastTags.Namespace("webhound")
+public class Webhound extends FastTags {
+
+    public static void _isMobile(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
+        if(args.containsKey("headers")) {
+            HashMap<String, ?> headers = (HashMap<String, ?>) args.get("headers");
+            String userAgent = headers.get("user-agent").toString();
+            String accept = headers.get("accept").toString();
+
+            UAgentInfo ua = new UAgentInfo(userAgent, accept);
+
+            if(ua.detectMobileLong()) {
+                out.println(JavaExtensions.toString(body));
+            }
+        }
+    }
+
+    public static void _isNotMobile(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
+        if(args.containsKey("headers")) {
+            HashMap<String, ?> headers = (HashMap<String, ?>) args.get("headers");
+            String userAgent = headers.get("user-agent").toString();
+            String accept = headers.get("accept").toString();
+
+            UAgentInfo ua = new UAgentInfo(userAgent, accept);
+
+            if(!ua.detectMobileLong()) {
+                out.println(JavaExtensions.toString(body));
+            }
+        }
+    }
+}
